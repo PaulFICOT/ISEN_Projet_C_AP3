4d8801a65ad6c71054603ab542e46858fe096e9a
Justin <justin.mottier@gmail.com>
2020-05-20 16:06:26 +0200

    initial commit

--- logic/includes/fixed_charge.h
+++ logic/includes/fixed_charge.h
@@ -1,12 +1,5 @@
-//
-// Created by justin on 15/05/2020.
-//
-
-#ifndef ISEN_PROJET_C_AP3_FIXED_CHARGE_H
-#define ISEN_PROJET_C_AP3_FIXED_CHARGE_H
+#include "position.h"
 
 struct fixed_charge {
 
 };
-
-#endif //ISEN_PROJET_C_AP3_FIXED_CHARGE_H
---
+++ logic/includes/position.h
@@ -0,0 +1,5 @@
+struct position {
+    float x;
+    float y;
+};
+
