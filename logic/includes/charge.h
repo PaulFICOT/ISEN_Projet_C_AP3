59b57590130fbb834b8267dd8a54e48149a9abea
Justin <justin.mottier@gmail.com>
2020-05-28 14:30:42 +0200

    added weight to the charges

--- logic/includes/charge.h
+++ logic/includes/charge.h
@@ -7,7 +7,8 @@ struct charge_ {
     coordinate position;
     symbol symbol;
     double force;
+    double weight;
     short is_fixed;
 };
 
-charge charge_create(double x, double y, symbol symbol, double force, short is_fixed);
+charge charge_create(double x, double y, symbol symbol, double force, double weight, short is_fixed);
--- logic/sources/charge.c
+++ logic/sources/charge.c
@@ -1,11 +1,12 @@
 #include "../includes/charge.h"
 
-charge charge_create(double x, double y, symbol symbol, double force, short is_fixed) {
+charge charge_create(double x, double y, symbol symbol, double force, double weight, short is_fixed) {
     charge ch;
     coordinate c = coordinate_create(x, y);
     ch.position = c;
     ch.symbol = symbol;
     ch.force = force;
+    ch.weight = weight;
     ch.is_fixed = is_fixed;
     return ch;
 }
