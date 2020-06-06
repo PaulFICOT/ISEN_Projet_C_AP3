c3a930654c534fa37fad07775540214920f9356a
justin <justin.mottier@gmail.com>
2020-06-06 18:09:19 +0200

    linked_list on steroids

--- logic/includes/linked_list.h
+++ logic/includes/linked_list.h
@@ -10,32 +10,146 @@ struct linked_list_
     linked_list *next;
 };
 
+/**
+ * Init a linked_list with every fields to NULL
+ * 
+ * @return a linked_list instance
+ */
+linked_list* null_linked_list();
+
+/**
+ * linked_list constructor.
+ * 
+ * @param n the value of the linked list
+ * @return a new linked_list instance
+ */
 linked_list* linked_list_create(void* n);
 
+/**
+ * Checks if the element is the last of the list (excluding the tail)
+ * 
+ * @param l the element to check
+ * 
+ * @return true if the element is the last
+ */
+short is_last(linked_list* l);
+
+/**
+ * Checks if the element is the first of the list (excluding the head)
+ * 
+ * @param l the element to check
+ * 
+ * @return true if the element is the first
+ */
+short is_first(linked_list* l);
+
+/**
+ * Checks if the element has a predecessor (including the head)
+ * 
+ * @param l the element to check
+ * 
+ * @return true if the element has a pedecessor
+ */
+short has_prev(linked_list* l);
+
+/**
+ * Checks if the element has a successor (including the tail)
+ * 
+ * @param l the element to check
+ * 
+ * @return true if the element has a successor
+ */
+short has_next(linked_list* l);
+
+/**
+ * Rewind the linked list
+ * 
+ * @param l a pointer to a linked_list's pointer
+ */
 void backtrack(linked_list **l);
 
+/**
+ * Appends an element at the end of the linked_list
+ * 
+ * @param l the list
+ * @param n the value of the new element
+ */
 void push(linked_list *l, void* n);
 
+/**
+ * Counts the number of occurences of an element in a linked_list
+ * 
+ * @param l the linked_list
+ * @param n the element to search
+ * 
+ * @return the number of occurences
+ */
 int count_occurrences(linked_list *l, void* n);
 
-void delete_int(linked_list **l, void* n, short rmAll);
-
+/**
+ * Append a linked list to another linked list
+ * 
+ * @param lA the linked list which will receive the other list
+ * @param lB the linked list whicj will be merged in the other
+ */
 void fusion(linked_list *lA, linked_list *lB);
 
+/**
+ * Count the number of elements in a linked list (exluding head and tail)
+ * 
+ * @param l the linked_list
+ * 
+ * @return the number of elements
+ */
 int length(linked_list *l);
 
+/**
+ * Swaps the element of a linked list with the next one
+ * 
+ * @param l the element to be swapped
+ */
 void swap_next(linked_list *l);
 
+/**
+ * print all the values of a linked lit of floats
+ * 
+ * @param l the linked list
+ */
 void print_float(linked_list *l);
 
+/**
+ * print all the values of a linked lit of strings
+ * 
+ * @param l the linked list
+ */
 void print_string(linked_list *l);
 
+/**
+ * print all the values of a linked lit of integers
+ * 
+ * @param l the linked list
+ */
 void print_int(linked_list *l);
 
+/**
+ * Deletes an element of a linked list
+ * 
+ * @param l a pointer to a linked_list's pointer
+ */
 void delete(linked_list **l);
 
+/**
+ * Move the pointer to the next element of the list
+ * 
+ * @param l a pointer to a linked_list's pointer
+ */
 void forward(linked_list **l, int n);
 
+/**
+ * Move the pointer to the previous element of the list
+ * 
+ * @param l a pointer to a linked_list's pointer
+ */
 void backward(linked_list **l, int n);
 
 #endif
--- logic/sources/linked_list.c
+++ logic/sources/linked_list.c
@@ -2,114 +2,178 @@
 #include <stdlib.h>
 #include "../includes/linked_list.h"
 
-linked_list* linked_list_create(void* n) {
+linked_list* null_linked_list() {
     linked_list *l = malloc(sizeof(linked_list));
     l->prev = NULL;
-    l->value = n;
+    l->value = NULL;
     l->next = NULL;
     return l;
+}
+
+linked_list* linked_list_create(void* n) {
+    linked_list *l = null_linked_list();
+
+    linked_list *head = null_linked_list();
+    head->next = l;
+    l->prev = head;
+
+    l->value = n;
+
+    linked_list *tail = null_linked_list();
+    tail->prev = l;
+    l->next = tail;
+    
+    return l;
+}
+
+short is_last(linked_list* l) {
+    return l->next->value == NULL;
+}
+
+short is_first(linked_list* l) {
+    return l->prev->value == NULL;
+}
+
+short has_prev(linked_list* l) {
+    return l->prev != NULL;
+}
+
+short has_next(linked_list* l) {
+    return l->next != NULL;
 }
 
 void backtrack(linked_list **l) {
     if (*l == NULL) {
         return;
     }
-    while ((*l)->prev != NULL) {
-        *l = (*l)->prev;
+    while (!is_first(*l)) {
+        backward(l, 1);
     }
 }
 
 void push(linked_list *l, void* n) {
-    while(l->next != NULL) {
-        l = l->next;
+    while(!is_last(l)) {
+        forward(&l, 1);
     }
-    linked_list *tmp = linked_list_create(n);
+    linked_list *tmp = null_linked_list();
     tmp->prev = l;
+    tmp->value = n;
+    tmp->next = l->next;
     l->next = tmp;
 }
 
 int count_occurrences(linked_list *l, void* n) {
     backtrack(&l);
     int counter = 0;
-    while (l != NULL) {
+    while (has_next(l)) {
         if (l->value == n) {
             counter++;
         }
-        l = l->next;
+        forward(&l, 1);
     }
     return counter;
 }
 
 void fusion(linked_list *lA, linked_list *lB) {
-    backtrack(&lA);
-    backtrack(&lB);
-    while (lA->next != NULL) {
-        lA = lA->next;
+    while (!is_last(lA)) {
+        forward(&lA, 1);
     }
+    backtrack(&lB);
     lA->next = lB;
+    lB->prev = lA;
 }
 
 int length(linked_list *l) {
     backtrack(&l);
     int counter = 0;
-    while (l != NULL) {
+    while (has_next(l)) {
         counter++;
-        l = l->next;
+        forward(&l, 1);
     }
     return counter;
 }
 
 void swap_next(linked_list *l) {
+    if (is_last(l)) {
+        return;
+    }
     void* tmp = l->value;
     l->value = l->next->value;
     l->next->value = tmp;
 }
 
 void print_float(linked_list *l) {
-    while (l != NULL) {
+    while (has_next(l)) {
         printf("%f\n", *((float*) l->value));
-        l = l->next;
+        forward(&l, 1);
     }
 }
 
 void print_string(linked_list *l) {
-    while (l != NULL) {
+    while (has_next(l)) {
         printf("%s\n", (char*) l->value);
-        l = l->next;
+        forward(&l, 1);
     }
 }
 
 void print_int(linked_list *l) {
-    while (l != NULL) {
+    while (has_next(l)) {
         printf("%i\n", *((int*) l->value));
-        l = l->next;
+        forward(&l, 1);
     }
 }
 
 void delete(linked_list **l) {
-    if ((*l)->prev == NULL) {
+    (*l)->next->prev = (*l)->prev;
+    (*l)->prev->next = (*l)->next;
+    if (is_first(*l)) {
         *l = (*l)->next;
-        (*l)->prev = (*l)->next;
-    } else if ((*l)->next == NULL) {
-        *l = (*l)->prev;
-        (*l)->next = (*l)->prev;
     } else {
-        (*l)->next->prev = (*l)->prev;
-        (*l)->prev->next = (*l)->next;
         *l = (*l)->prev;
     }
 }
 
 void forward(linked_list **l, int n) {
-    while (n > 0 && *l != NULL) {
+    while (n > 0 && has_next(*l)) {
         *l = (*l)->next;
         n--;
     }
 }
 
 void backward(linked_list **l, int n) {
-    while (n > 0 && *l != NULL) {
+    while (n > 0 && has_prev(*l)) {
         *l = (*l)->prev;
         n--;
     }
 }
+
+// int main() {
+//     int a = 1;
+//     int b = 8;
+//     int c = 2;
+//     int d = 6;
+
+//     linked_list* l = linked_list_create(&a);
+//     push(l, &b);
+//     push(l, &c);
+//     push(l, &d);
+
+//     int e = 7;
+//     int f = 4;
+//     int g = 1;
+//     int h = 5;
+//     linked_list* m = linked_list_create(&e);
+//     push(m, &f);
+//     push(m, &g);
+//     push(m, &h);
+
+//     fusion(l, m);
+//     forward(&l, length(l) - 1);
+//     delete(&l);
+//     backtrack(&l);
+//     printf("while\n");
+//     while (has_next(l)) {
+//         printf("%i\n", * (int*) l->value);
+//         forward(&l, 1);
+//     }
+// }
