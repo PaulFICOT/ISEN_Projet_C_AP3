3ca2e9e14b0b15412a19874e551d69bbf0a2f6b5
justin <justin.mottier@gmail.com>
2020-06-12 13:58:50 +0200

    new coulomb law

--- logic/sources/vector.c
+++ logic/sources/vector.c
@@ -3,23 +3,22 @@
 #include <math.h>
 #include <stdlib.h>
 
-vector* vector_create(coordinate* s, coordinate* e) {
+vector* vector_create(coordinate* s, coordinate* e, double magnitude) {
     vector* v = malloc(sizeof(vector));
     v->start = s;
     v->end = e;
+    v->magnitude = magnitude;
     return v;
 }
 
-vector* vector_create_from_straight_line(coordinate* s, double slope, double magnitude) {
-    coordinate* e = malloc(sizeof(coordinate));
-    e->x = magnitude / slope;
-    e->y = slope * e->x + (-slope * s->x + s->y);
+vector* vector_create_from_straight_line(coordinate* s, double magnitude, double slope) {
+    double slope_angle = atan(slope);
+    int symbol = magnitude < 0 ? 1 : -1;
+    printf("slope: %f - magnitude: %f\n", slope, magnitude * pow(10, 9));
 
-    return vector_create(s, e);
-}
-
-double calculate_magnitude(vector* v) {
-    return two_points_distance(v->start, v->end);
+    double delta_x = magnitude * (slope < 0 ? cos(slope_angle) : sin(slope_angle)) * symbol;
+    double delta_y = magnitude * (slope < 0 ? sin(slope_angle) : cos(slope_angle)) * symbol;
+    return vector_create(s, coordinate_create(s->x - delta_x, s->y - delta_y), magnitude);
 }
 
 double calculate_slope(coordinate* a, coordinate* b) {
