09af845aa760b8e6dc3660365f887ea90296405c
justin <justin.mottier@gmail.com>
2020-06-14 23:51:46 +0200

    rand accordingly to create charge

--- logic/sources/charge.c
+++ logic/sources/charge.c
@@ -30,8 +30,8 @@ charge* random_charge(short is_fixed) {
         rand() % 2000 * pow(10, -2) * (rand() % 2 == 1 ? -1 : 1),
         rand() % 1200 * pow(10, -2) * (rand() % 2 == 1 ? -1 : 1),
         rand() % 2 == 1 ? POSITIVE : NEGATIVE,
-        rand() % 1100 * pow(10, -2) * (rand() % 2 == 1 ? -1 : 1),
-        ((rand() % 9000) + 1) * pow(10,-34),
+        rand() % 110 * pow(10, -1) * (rand() % 2 == 1 ? -1 : 1),
+        ((rand() % 900) + 1) * pow(10,-33),
         is_fixed
     );
 }
