d4d4a23db8593224e6be2b9ce31553e357ccf62a
justin <justin.mottier@gmail.com>
2020-06-16 18:55:44 +0200

    start function does it's job

--- logic/sources/charge.c
+++ logic/sources/charge.c
@@ -30,8 +30,8 @@ charge* random_charge(short is_fixed) {
         rand() % 2000 * pow(10, -2) * (rand() % 2 == 1 ? -1 : 1),
         rand() % 1200 * pow(10, -2) * (rand() % 2 == 1 ? -1 : 1),
         rand() % 2 == 1 ? POSITIVE : NEGATIVE,
-        rand() % 10 * pow(10,-20) * (rand() % 2 == 1 ? -1 : 1),
-        ((rand() % 900) + 1) * pow(10,-33),
+        rand() % 10 * pow(10,-18) * (rand() % 2 == 1 ? -1 : 1),
+        ((rand() % 900) + 1) * pow(10,-12),
         is_fixed
     );
 }
