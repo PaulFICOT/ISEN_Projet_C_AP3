adafcbd8b464019984f77d9647407c708f4ee5e6
Paul <paul.ficot@gmail.com>
2020-06-19 18:18:08 +0200

    Header comments FINAL

--- logic/sources/distance_converter.c
+++ logic/sources/distance_converter.c
@@ -1,9 +1,19 @@
 #include "../includes/distance_converter.h"
 
+/**
+ * Converts nanometers to meters
+ * 
+ * @param nm a distance in nanometers
+ **/
 double nm_to_m(double nm) {
     return nm * pow(10, -9);
 }
 
+/**
+ * Converts meters to nanometers
+ * 
+ * @param m a distance in meters
+ **/
 double m_to_nm(double m) {
     return m * pow(10, 9);
 }
