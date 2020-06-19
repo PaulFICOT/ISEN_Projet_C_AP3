a10ca2be795733de643061b1ad842477482b93ab
Paul <paul.ficot@gmail.com>
2020-06-19 18:21:36 +0200

    Final ?

--- logic/includes/distance_converter.h
+++ logic/includes/distance_converter.h
@@ -3,7 +3,17 @@
 
 #include <math.h>
 
+/**
+ * Converts nanometers to meters
+ * 
+ * @param nm a distance in nanometers
+ **/
 double nm_to_m(double nm);
 
+/**
+ * Converts meters to nanometers
+ * 
+ * @param m a distance in meters
+ **/
 double m_to_nm(double m);
 #endif
